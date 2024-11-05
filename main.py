e2563b7d0b1db1a16437e4035ccd83aae6e1058f
Admin <laurentiu.binig@gmail.com>
2024-11-05 13:24:04 +0000

    comit

---
+++ main.py
@@ -0,0 +1,41 @@
+i = 1
+while i < 6:
+    print(i)
+    if i == 3:
+        break
+    i +=1
+
+
+fruits = ["apple", "banana" , "cherry"]
+for x in fruits:
+    print(x)
+
+
+for x in "banana":
+    print(x)
+
+
+fruits =["apple", "banana" , "cherry"]
+for x in fruits:
+    print(x)
+    if x =="banana":
+        break
+
+for x in range(6):
+    print(x)
+
+for x in range(2, 30, 6):
+    print(x)
+
+adj =["red", "big", "tasty"]
+fruits =["apple", "banana", "cherry"]
+
+for x in adj:
+    for y in fruits:
+        print(x, y)
+
+
+for x in ["0, 1, 2"]:
+    pass
+
+
